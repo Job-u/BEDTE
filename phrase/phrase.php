6f8c89daeb302a7ea120e10368b9522ff93bddf6
Jobu <111178322+Job-u@users.noreply.github.com>
2024-12-10 15:07:17 +0800

    Update phrase.php

--- phrase/phrase.php
+++ phrase/phrase.php
@@ -18,64 +18,56 @@
 </head>
 <body>
 
-    <section class="background" id="home">
-        <nav class="navbar">
-            <div class="logo">
-                <a href="../userdashboard/userdashboard.php"><img src="../img/back.png" alt=""></a>
-                <span>BEDTE</span>
-            </div>
-            <ul class="nav_items" id="nav_links">
+<nav class="navbar">
+        <a href="../userdashboard/userdashboard.php"><div class="logo">
+            <img src="../img/LOGO.png" alt="">
+            <span>BEDTE</span>
+        </div></a>
+        <ul class="nav_items" id="nav_links">
                 <div class="item">
-                    <li><a href="#">The Developers</a></li>
-                    <li><a href="#">Contact Us</a></li>
+            <li><a href="../aboutus/aboutus.php">Project Team</a></li>
+            <li><a href="../contact/contact.html">Contact Us</a></li>
                 </div>
-
+    
                 <?php 
+                
                 $id = $_SESSION['id'];
                 $query = mysqli_query($con,"SELECT*FROM users WHERE Id=$id");
-
+    
                 while($result = mysqli_fetch_assoc($query)){
                     $res_Uname = $result['Username'];
                     $res_Email = $result['Email'];
                     $res_Age = $result['Age'];
                     $res_id = $result['Id'];
                 }
+                
                 echo "<a href='../profile/edit.php?Id=$res_id'>$res_Uname</a>";
                 ?>
-
-                <div class="nav_btn">
-                    <a href="../phpsql/logout.php"><button class="btn btn2">Logout</button></a>
-                </div>
-            </ul>
-            <div class="nav_menu" id="menu_btn">
-                <i class="ri-menu-line"></i>
+    
+            <div class="nav_btn">
+            <a href="../phpsql/logout.php"><button class="btn btn2">Logout</button></a>
             </div>
-        </nav>
-    </section>
+        </ul>
+        <div class="nav_menu" id="menu_btn">
+            <i class="ri-menu-line"></i>
+        </div>
+    </nav>
 
     <section class="sec" id="features">
         <div class="left">
-            <h1>Hello, <b><?php echo $res_Uname ?></b>!</p></h1>
+            <h1>Phrase of the Day</h1>
             
         </div>
         
-        <div class="box" id="phrases">
-   <div class="head">
-      <h1 class="heading">Word of the Day</h1>
-   </div>     
+                
 
    <!-- This is where the word of the day will be displayed -->
-   <p id="daily-phrase" class="phrase-text"></p>
+   <p id="daily-phrase" class="heading , phrase-texta"></p>
 </div>
 
     </section>
 
-    <footer>
-        <div class="sec footer_container"></div>
-        <div class="footer_bar">
-            copyright @ 2024 BEDTE Developers. All rights reserved
-        </div>
-    </footer>
+
     <script src="https://unpkg.com/scrollreveal"></script>
     <script src="../js/phrase.js"></script>
 </body>
