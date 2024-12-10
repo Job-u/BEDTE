9bf34bb93bd9474e6bf9f33faa67c78b3e43e957
Jobu <111178322+Job-u@users.noreply.github.com>
2024-12-10 15:02:18 +0800

    Update aboutus.php

--- aboutus/aboutus.php
+++ aboutus/aboutus.php
@@ -1,175 +1,91 @@
-<<?php 
-   session_start();
-
-   include("../phpsql/config.php");
-   if(!isset($_SESSION['valid'])){
-    header("Location: login.php");
-   }
-?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!--Font Awesome CDN-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <title>Project Team</title>
-    <link rel="stylesheet" href="../style/aboutus.css">
-</head>
-<body>
-
-    <nav class="navbar">
-        <a href="../userdashboard/userdashboard.php"><div class="logo">
-            <img src="../img/LOGO.png" alt="">
-            <span>BEDTE</span>
-        </div></a>
-        <ul class="nav_items" id="nav_links">
-                <div class="item">
-            <li><a href="../aboutus/aboutus.html">Project Team</a></li>
-            <li><a href="../contact/contact.html">Contact Us</a></li>
-                </div>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible"content="IE=edge">
+        <meta name="viewport"content="widt=device-width, initial-scale=1.0">
+        <title>BEDTE Quiz  </title>
+        <link rel="stylesheet" href="../style/gamestyle.css">
     
-                <?php 
+    </head>
+    <body>
+    
+        <main class="main">
+            <header class="header">
+                <a href="../userdashboard/userdashboard.php"class="logo">BEDTE,</a>
+    
+                <nav class="navbar">
+                    <a href="../aboutus/aboutus.php">about</a>
+                    <a href="../contact/contact.html">contact</a>
+                </nav>
+            </header>
+    
+            <div class="container">
+                <section class=" quiz-section">
+                   <div class="quiz-box">
+                        <h1 id="title">TITLE  QUIZ</h1>
+                        <div class="quiz-header">
+                            <span id ="diffcuLty-label ">EASY</span>
+                            <span class="header-score">Score: 0 / 0 </span>
+                        </div>
+                        <h2 class="question-text">what is a cat? </h2>
+                        <div class="option-list"></div>
+                        <div class="quiz-footer">
+                        <span class="question-total">1 of 0 Questions</span>
+                        <button class="next-btn">Next</button>
+                        </div>
+
+                        <div class="popup" id="message">
+                            <div class="pup-content"> 
+                                <h3 id="pupMessage"> Message</h3>
+                                <button class="pupCloseBtn">Close</button>
+                    
+                         </div>
+                      </div>
+                    </div>
+                    <div class="result-box">
+                        <h2>Quiz Result!</h2>
+                        <div class="percentage-container">
+                            <div class="circular-progress">
+                                <span class="progress-value">0%</span>
+                            </div>
+    
+                            <span class="score-text">Your Score 0 out of 0 </span>
+                        </div>
+                        <div class="buttons">
+                            <button class="TryAgain-btn">Try Again</button>
+                            <button class="gohome-btn">Go To Home</button>
+                        </div>
+                    </div>
+          </section>
+     <section class="home">
+            <div class="home-content">
+                <h1>SELECT MODE</h1>
                 
-                $id = $_SESSION['id'];
-                $query = mysqli_query($con,"SELECT*FROM users WHERE Id=$id");
-    
-                while($result = mysqli_fetch_assoc($query)){
-                    $res_Uname = $result['Username'];
-                    $res_Email = $result['Email'];
-                    $res_Age = $result['Age'];
-                    $res_id = $result['Id'];
-                }
+                <div class="mode-btn">
+                <button class="ho-btn multi-btn">Multiple Quiz </button>
+                <button class="ho-btn fill-btn "> Fill in the blank Quiz</button>
                 
-                echo "<a href='../profile/edit.php?Id=$res_id'>$res_Uname</a>";
-                ?>
+            </div> 
+            </div>    
+         </section>
+        </div>
+     </main>
     
-            <div class="nav_btn">
-            <a href="../phpsql/logout.php"><button class="btn btn2">Logout</button></a>
-            </div>
-        </ul>
-        <div class="nav_menu" id="menu_btn">
-            <i class="ri-menu-line"></i>
+        <div class="popup-info">
+            <h2>Select Level</h2>
+            <div class="sel-btn">
+            <button class="info-btn easy-btn ">EASY</button>
+            <button class="info-btn medium-btn ">MEDIUM</button>
+            <button class="info-btn hard-btn ">HARD</button>
+        <div class="btn-exit">
+            <button class="info-btn exit-btn">Exit Quiz</button>
+
         </div>
-    </nav>
-
-
-    <div class="wrapper">
-
-        <div class="title">
-            <h4>Our Team Section</h4>
-        </div>
-
-        <div class="card_Container">
-
-            <div class="card">
-
-                <div class="imbBx">
-                    <img src="../img/pic1.jpg" alt="">
-                </div>
-
-                <div class="content">
-                    <div class="contentBx">
-                        <h3>Joemari Antonio Shane <br><span>Lead Nigger</span></h3>
-                    </div>
-                    <ul class="sci">
-                        <li style="--i: 1">
-                            <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                        </li>
-                        <li style="--i: 2">
-                            <a href="#"><i class="fa-brands fa-github"></i></a>
-                        </li>
-                        <li style="--i: 3">
-                            <a href="https://www.facebook.com/Shane.cutieehehe" target="_blank"><i class="fa-brands fa-facebook"></i></a>
-                        </li>
-                    </ul>
-                </div>
-
-            </div>
-
-            <div class="card">
-
-                <div class="imbBx">
-                    <img src="../img/pic1.jpg" alt="">
-                </div>
-
-                <div class="content">
-                    <div class="contentBx">
-                        <h3>Job Montenegro <br><span>Assistant Programmer </span></h3>
-                    </div>
-                    <ul class="sci">
-                        <li style="--i: 1">
-                            <a href="https://www.instagram.com/montenegrojob_/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
-                        </li>
-                        <li style="--i: 2">
-                            <a href="https://github.com/Job-u" target="_blank"><i class="fa-brands fa-github"></i></a>
-                        </li>
-                        <li style="--i: 3">
-                            <a href="https://www.facebook.com/job.montenegro.33/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
-                        </li>
-                    </ul>
-                </div>
-
-            </div>
-
-            <div class="card">
-
-                <div class="imbBx">
-                    <img src="../img/pic1.jpg" alt="">
-                </div>
-
-                <div class="content">
-                    <div class="contentBx">
-                        <h3>Daniel Denzel Tulio <br><span>System Analyst </span></h3>
-                    </div>
-                    <ul class="sci">
-                        <li style="--i: 1">
-                            <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                        </li>
-                        <li style="--i: 2">
-                            <a href="#"><i class="fa-brands fa-github"></i></a>
-                        </li>
-                        <li style="--i: 3">
-                            <a href="https://www.facebook.com/DanielDenzelPogi" target="_blank"><i class="fa-brands fa-facebook"></i></a>
-                        </li>
-                    </ul>
-                </div>
-
-            </div>
-
-            <div class="card">
-
-                <div class="imbBx">
-                    <img src="../img/pic1.jpg" alt="">
-                </div>
-
-                <div class="content">
-                    <div class="contentBx">
-                        <h3>Richie Florida <br><span>Designer</span></h3>
-                    </div>
-                    <ul class="sci">
-                        <li style="--i: 1">
-                            <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                        </li>
-                        <li style="--i: 2">
-                            <a href="#"><i class="fa-brands fa-github"></i></a>
-                        </li>
-                        <li style="--i: 3">
-                            <a href="https://www.facebook.com/chieflorida19" target="_blank"><i class="fa-brands fa-facebook"></i></a>
-                        </li>
-                    </ul>
-                </div>
-
-            </div>
-        </div>
-
     </div>
     
-    <script src="https://unpkg.com/scrollreveal"></script>
-    <script src="../js/homepage.js"></script>
-</body>
-
-
-</html>
+    
+    <script src="../js/gamequestion.js"></script>
+    <script src="../js/gamescript.js"></script>
+    </body>
+    </html>
